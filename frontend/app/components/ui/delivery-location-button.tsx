79f6f9c1ac294b7ed43c8ff294a13e78a3a34c3a
Anwar S. <anwar.sanchz@gmail.com>
2024-12-21 22:08:25 -0600

    Feat: delivery button (#77)

---
+++ frontend/app/components/ui/delivery-location-button.tsx
@@ -0,0 +1,38 @@
+import * as React from "react"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select"
+
+
+export function DeliveryLocationButton() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a country" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Countries</SelectLabel>
+        </SelectGroup>
+        <SelectItem value="cr" className="flex items-center justify-between">
+            Costa Rica         
+            <img 
+              src="/images/costarica.svg" 
+              alt="Costa Rica flag"
+              className="inline-block w-6 h-6 ml-2" 
+            />
+          </SelectItem>
+      </SelectContent>
+    </Select>
+  )
+}
+
+
+export default DeliveryLocationButton
+

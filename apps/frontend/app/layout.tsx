0c1f5b5cab655ab033ee21427288a612567bf35d
Olamiposi <tundexsmith624@gmail.com>
2025-01-22 18:49:07 +0100

    style: typography update (#101)

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -4,14 +4,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./components/providers/theme-provider";
 
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
+const satoshi = localFont({
+	src: "./fonts/Satoshi.woff2",
+	variable: "--font-satoshi",
 	weight: "100 900",
 });
 
@@ -27,9 +22,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${satoshi.variable} antialiased`}>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"

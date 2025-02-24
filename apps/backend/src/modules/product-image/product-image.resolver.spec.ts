2b1bb68446eaf3c17905d773832dd2232ebc5bcc
Abdulhakeem Abdulazeez Ayodeji <44169294+Ayoazeez26@users.noreply.github.com>
2025-02-24 23:21:10 +0100

    Test/product image (#185)
    * test: wrote unit tests for product-image.service.ts
    
    * test: added unit test for product-image.resolver.ts
    
    * feat: cleanup
    
    * fix: fixed lint errors

--- apps/backend/src/modules/product-image/product-image.resolver.spec.ts
+++ apps/backend/src/modules/product-image/product-image.resolver.spec.ts
@@ -1,10 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "src/core/prisma/prisma.service";
+import { ProductImageDTO } from "./dto/product-image.input";
+import { ProductImage } from "./entities/product-image.entity";
 import { ProductImageResolver } from "./product-image.resolver";
 import { ProductImageService } from "./product-image.service";
 
 describe("ProductImageResolver", () => {
 	let resolver: ProductImageResolver;
+
+	const mockProductImageService = {
+		createProductImage: jest.fn(),
+		getAllProductImages: jest.fn(),
+		getAProductImage: jest.fn(),
+	};
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -12,14 +20,8 @@ describe("ProductImageResolver", () => {
 				ProductImageResolver,
 				ProductImageService,
 				{
-					provide: PrismaService,
-					useValue: {
-						productImage: {
-							findMany: jest.fn(),
-							findUnique: jest.fn(),
-							create: jest.fn(),
-						},
-					},
+					provide: ProductImageService,
+					useValue: mockProductImageService,
 				},
 			],
 		}).compile();
@@ -29,5 +31,63 @@ describe("ProductImageResolver", () => {
 
 	it("should be defined", () => {
 		expect(resolver).toBeDefined();
+	});
+
+	it("should create a product image", async () => {
+		const data: ProductImageDTO = {
+			imageUrl: "http://image.com",
+			productId: "1",
+		};
+
+		const createdImage = {
+			id: "1",
+			...data,
+		};
+
+		mockProductImageService.createProductImage.mockReturnValue(createdImage);
+
+		const result = await resolver.createProductImage(data);
+
+		expect(mockProductImageService.createProductImage).toHaveBeenCalled();
+		expect(mockProductImageService.createProductImage).toHaveBeenCalledWith(
+			data,
+		);
+
+		expect(result).toEqual(createdImage);
+	});
+
+	it("should return an array of all product images", async () => {
+		const productImages = [
+			{
+				id: "1",
+				imageUrl: "http://image.com",
+				productId: "1",
+			},
+		];
+
+		mockProductImageService.getAllProductImages.mockReturnValue(productImages);
+
+		const result = await resolver.productImages();
+
+		expect(mockProductImageService.getAllProductImages).toHaveBeenCalled();
+
+		expect(result).toEqual(productImages);
+	});
+
+	it("should return a product image", async () => {
+		const productImage = {
+			id: "1",
+			imageUrl: "http://image.com",
+			productId: "1",
+		};
+
+		mockProductImageService.getAProductImage.mockReturnValue(productImage);
+
+		const result = await resolver.productImage("1");
+
+		expect(mockProductImageService.getAProductImage).toHaveBeenCalled();
+		expect(mockProductImageService.getAProductImage).toHaveBeenCalledWith("1");
+
+		expect(result).toEqual(productImage);
 	});
 });
--- apps/backend/src/modules/product-image/product-image.service.spec.ts
+++ apps/backend/src/modules/product-image/product-image.service.spec.ts
@@ -1,9 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "src/core/prisma/prisma.service";
+import { ProductImageDTO } from "./dto/product-image.input";
 import { ProductImageService } from "./product-image.service";
 
 describe("ProductImageService", () => {
 	let service: ProductImageService;
+
+	const mockPrismaService = {
+		productImage: {
+			create: jest.fn(),
+			findMany: jest.fn(),
+			findUnique: jest.fn(),
+		},
+	};
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -11,13 +20,7 @@ describe("ProductImageService", () => {
 				ProductImageService,
 				{
 					provide: PrismaService,
-					useValue: {
-						productImage: {
-							findMany: jest.fn(),
-							findUnique: jest.fn(),
-							create: jest.fn(),
-						},
-					},
+					useValue: mockPrismaService,
 				},
 			],
 		}).compile();
@@ -27,5 +30,87 @@ describe("ProductImageService", () => {
 
 	it("should be defined", () => {
 		expect(service).toBeDefined();
+	});
+
+	it("should create a product image", async () => {
+		const data: ProductImageDTO = {
+			imageUrl: "http://image.com",
+			productId: "1",
+		};
+
+		const createdImage = {
+			id: "1",
+			...data,
+		};
+
+		mockPrismaService.productImage.create.mockReturnValue(createdImage);
+
+		const result = await service.createProductImage(data);
+
+		expect(mockPrismaService.productImage.create).toHaveBeenCalled();
+		expect(mockPrismaService.productImage.create).toHaveBeenCalledWith({
+			data,
+		});
+
+		expect(result).toEqual(createdImage);
+	});
+
+	it("should return an array of all product images", async () => {
+		const productImages = [
+			{
+				id: "1",
+				imageUrl: "http://image.com",
+				productId: "1",
+			},
+		];
+
+		mockPrismaService.productImage.findMany.mockReturnValue(productImages);
+
+		const result = await service.getAllProductImages();
+
+		expect(result).toEqual(productImages);
+		expect(mockPrismaService.productImage.findMany).toHaveBeenCalled();
+	});
+
+	it("should return an empty array if no product images are found", async () => {
+		mockPrismaService.productImage.findMany.mockReturnValue([]);
+
+		const result = await service.getAllProductImages();
+
+		expect(result).toEqual([]);
+		expect(mockPrismaService.productImage.findMany).toHaveBeenCalled();
+	});
+
+	it("should get a product image", async () => {
+		const id = "1";
+		const data = {
+			id: "1",
+			imageUrl: "http://image.com",
+			productId: "1",
+		};
+
+		mockPrismaService.productImage.findUnique.mockReturnValue(data);
+
+		const result = await service.getAProductImage(id);
+
+		expect(result).toEqual(data);
+		expect(mockPrismaService.productImage.findUnique).toHaveBeenCalled();
+		expect(mockPrismaService.productImage.findUnique).toHaveBeenCalledWith({
+			where: { id },
+		});
+	});
+
+	it("should throw an error if product image is not found", async () => {
+		const id = "1";
+
+		mockPrismaService.productImage.findUnique.mockReturnValue(null);
+
+		await expect(service.getAProductImage(id)).rejects.toThrow(
+			"Product image not found.",
+		);
+		expect(mockPrismaService.productImage.findUnique).toHaveBeenCalled();
+		expect(mockPrismaService.productImage.findUnique).toHaveBeenCalledWith({
+			where: { id },
+		});
 	});
 });
